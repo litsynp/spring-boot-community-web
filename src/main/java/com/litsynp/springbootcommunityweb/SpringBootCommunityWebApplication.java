c0bb27b24829a7ebe5c9c601e7f46bee8013aa9e
litsynp <nocte55is@gmail.com>
2022-06-02 20:02:50 +0900

    build: init project

---
+++ src/main/java/com/litsynp/springbootcommunityweb/SpringBootCommunityWebApplication.java
@@ -0,0 +1,12 @@
+package com.litsynp.springbootcommunityweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCommunityWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCommunityWebApplication.class, args);
+    }
+}
---
+++ src/test/java/com/litsynp/springbootcommunityweb/SpringBootCommunityWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.litsynp.springbootcommunityweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommunityWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
